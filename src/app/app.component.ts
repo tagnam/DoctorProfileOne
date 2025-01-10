a2a0ce85cddc8ae3a6816003bc7142c99ad2c7f9
tagnam <ravi@tagnamcommunications.com>
2025-01-10 16:26:28 +0530

    project upload on github and netlify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    DoctorComponent,
+    ServicesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
